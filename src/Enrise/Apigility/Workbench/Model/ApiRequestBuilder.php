f58bb1ab256a78a634dac19cab4f5a5f84e6749a
Jeroen van Dijk <jeroen@enrise.com>
2013-11-19 20:53:29 +0100

    Add ability to consume API

--- src/Enrise/Apigility/Workbench/Model/ApiRequestBuilder.php
+++ src/Enrise/Apigility/Workbench/Model/ApiRequestBuilder.php
@@ -14,12 +14,20 @@ class ApiRequestBuilder
 
         $proxyData = $proxyRequest->getPost();
 
+        $uri = $proxyData['core']['path'];
+        foreach ($proxyData['params'] as $key => $value) {
+            $uri = str_replace(':'.$key, $value, $uri);
+        }
+        $uri = str_replace(array('[',']'), array('',''), $uri);
+
         $headers = new \Zend\Http\Headers();
+        $apiRequest->setMethod($proxyData['core']['http_method']);
+        $apiRequest->setHeaders($headers->fromString('Accept: '.$proxyData['core']['accept']));
+        $apiRequest->setHeaders($headers->fromString('Content-Type: '.$proxyData['core']['content_type']));
 
+        $apiRequest->setUri($proxyData['core']['host'] . $uri);
 
-        $apiRequest->setHeaders($headers->fromString('Accept: application/json'));
-
-        $apiRequest->setUri($proxyData['baseuri'] . $proxyData['endpoint']);
+        var_dump($apiRequest);
 
         return $apiRequest;
     }
