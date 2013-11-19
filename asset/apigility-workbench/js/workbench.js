a4953f724b4226b69a7480f4d1637806d12d5f6c
Frank de Weger <frankdeweger@gmail.com>
2013-11-19 20:03:12 +0100

    Include first JS file

--- asset/apigility-workbench/js/workbench.js
+++ asset/apigility-workbench/js/workbench.js
@@ -1,35 +1,4 @@
 $(document).ready(function() {
     console.log("start");
-    var parsedRoutes = parseRoutes(routes, zf_rest);
-    initResources(parsedRoutes);
 });
 
-function initResources(parsedRoutes)
-{
-    var options = [];
-    for (var route in parsedRoutes)
-    {
-        options.push($('<option>'  + parsedRoutes[route].name + '</option>'));
-    }
-    console.log(options);
-    $('#resources').append(options);
-}
-
-function parseRoutes(routes, zf_rest)
-{
-    var parsedRoutes = [];
-    for (route in routes)
-    {
-        name = route;
-        route = routes[route];
-
-        var parsedRoute = {
-            "name": name,
-            "route": route
-        }
-
-        parsedRoutes.push(parsedRoute);
-    }
-
-    return parsedRoutes;
-}
